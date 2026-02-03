80c974f6d200e7861ce9655441c39588b189cb39
vinig <vinigmonster@gmail.com>
2026-02-03 20:11:31 -0300

    divine potion now is really dinive

--- src/main/java/net/vinicius/tutorialmod/item/ModFoodComponents.java
+++ src/main/java/net/vinicius/tutorialmod/item/ModFoodComponents.java
@@ -10,10 +10,23 @@ public class ModFoodComponents {
     public static final FoodComponent CAULIFLOWER = new FoodComponent.Builder()
             .nutrition(3)
             .saturationModifier(0.25f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 200,0), 0.15f).build();
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 200,0), 0.15f)
+            .build();
 
     public static final FoodComponent DIVINE_POTION = new FoodComponent.Builder()
             .nutrition(0)
             .saturationModifier(0)
-            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 200,1), 1.0f).build();
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 200,5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200,5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200,2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 200,0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 200,5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 200,0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 200,0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 200,5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HERO_OF_THE_VILLAGE, 200,1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 200,5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 200,2), 1.0f)
+            .alwaysEdible()
+            .build();
 }
